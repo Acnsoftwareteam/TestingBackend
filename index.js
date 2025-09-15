2522d1a48c61f586f6c7c60e83b7c59c39a0ab2d
Ashutosh Lakshakar <AshutoshL@acnhealthcare.com>
2025-09-15 16:54:27 +0530

    feat:tested

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+// Root route
+
+app.get("/", (req, res) => {
+  res.send("Hello, this is my first Node.js app with one route!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
